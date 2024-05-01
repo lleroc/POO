0cab6ab78f8bddc2f19d826c45b9580f8bcd74b9
Lleroc <llleroc@yahoo.es>
2024-05-01 10:55:12 -0500

    DoWhile

--- index.js
+++ index.js
@@ -13,37 +13,47 @@ var Pera = require("./Frutas/Pera");
 
 console.log("Bienvenido a la Fruteria XXX");
 console.log("Escriba el numero de la opción que desea guradar");
-console.log("*** MENU ***");
-console.log("1) Claudia");
-console.log("2) Fresa");
-console.log("3) Mango");
-console.log("4) Manzana");
-console.log("5) Mora");
-console.log("6) Pera");
-//La elección de la opción
-var opcion = teclado("Ingrese una opción: ");
-//Información de la fruta
-var id = teclado("Ingrese el numero de fruta: ");
-var peso = teclado("Ingrese el peso: ");
-var color = teclado("Ingrese el color: ");
-var estado = teclado("Ingrese el estado: ");
+do {
+  console.log("*** MENU ***");
+  console.log("1) Claudia");
+  console.log("2) Fresa");
+  console.log("3) Mango");
+  console.log("4) Manzana");
+  console.log("5) Mora");
+  console.log("6) Pera");
+  console.log("7) Lista de Frutas");
+  //La elección de la opción
+  var opcion = teclado("Ingrese una opción: ");
+  //Información de la fruta
+  if (opcion != 7) {
+    var id = teclado("Ingrese el numero de fruta: ");
+    var peso = teclado("Ingrese el peso: ");
+    var color = teclado("Ingrese el color: ");
+    var estado = teclado("Ingrese el estado: ");
+  }
 
-if (opcion == 1) {
-  var claudia = new Claudia(id, peso, color, estado);
-  frutas.guardarFruta(claudia);
-} else if (opcion == 2) {
-  var fresa = new Fresa(id, peso, color, estado);
-  frutas.guardarFruta(fresa);
-} else if (opcion == 3) {
-  var mango = new Mango(id, peso, color, estado);
-  frutas.guardarFruta(mango);
-} else if (opcion == 4) {
-  var manzana = new Manzana(id, peso, color, estado);
-  frutas.guardarFruta(manzana);
-} else if (opcion == 5) {
-  var mora = new Mora(id, peso, color, estado);
-  frutas.guardarFruta(mora);
-} else {
-  var pera = new Pera(id, peso, color, estado);
-  frutas.guardarFruta(pera);
-}
+  if (opcion == 1) {
+    var claudia = new Claudia(id, peso, color, estado);
+    frutas.guardarFruta(claudia);
+  } else if (opcion == 2) {
+    var fresa = new Fresa(id, peso, color, estado);
+    frutas.guardarFruta(fresa);
+  } else if (opcion == 3) {
+    var mango = new Mango(id, peso, color, estado);
+    frutas.guardarFruta(mango);
+  } else if (opcion == 4) {
+    var manzana = new Manzana(id, peso, color, estado);
+    frutas.guardarFruta(manzana);
+  } else if (opcion == 5) {
+    var mora = new Mora(id, peso, color, estado);
+    frutas.guardarFruta(mora);
+  } else if (opcion == 6) {
+    var pera = new Pera(id, peso, color, estado);
+    frutas.guardarFruta(pera);
+  } else {
+    console.log(frutas.obtenerFrutas());
+  }
+
+  var salir = teclado("Desea ingresar otra fruta Si   No");
+} while (salir == "Si");
+console.log("Gracias por su visita");
